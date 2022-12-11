6e49e6173062e82c6aabcb95d1ff09015da8c87a
Roman Movchan <movchan@gmail.com>
2022-12-11 21:21:30 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learning Python
+
+1. Clone repository
+2. Run
+```bash
+$ python menu.py
+```
+
---
+++ menu.py
@@ -0,0 +1,7 @@
+print("[1]. data structures (list, dict, tuple, set)")
+print("[2]. functions")
+print("[3]. filesystem operations")
+print("[4]. exceptions")
+print("[5]. objects details and abstractions")
+print("[6]. database")
+print("[7]. web application")
