3d7f273ab5c60ad6e2a3f24244e4d4488ed676ec
mvchn <movchan@gmail.com>
2022-12-14 22:01:39 +0200

    add data file

---
+++ data.py
@@ -0,0 +1,16 @@
+book = [1, 'learn python hard way', 'zed shaw', 2021]
+print(book)
+
+book = {
+    'id': 1,
+    'title': 'learn python hard way',
+    'author': 'zed shaw',
+    'year': 2021
+}
+print(book)
+
+book = {1, 'learn python hard way', 'zed shaw', 2021}
+print(book)
+
+book = (1, 'learn python hard way', 'zed shaw', 2021)
+print(book)
