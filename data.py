8bded1d684f3a20ee6c28b5acc97e9a05308d97a
mvchn <movchan@gmail.com>
2022-12-18 18:03:09 +0200

    lesson changes

--- data.py
+++ data.py
@@ -1,6 +1,18 @@
-book = [1, 'learn python hard way', 'zed shaw', 2021]
+headers = ['id', 'title', 'author', 'year']
+
+# list
+book = [1, 'learn python hard way', 'zed shaw', 2021, 'zed shaw']
 print(book)
 
+# tuple
+book = (1, 'learn python hard way', 'zed shaw', 2021, 'zed shaw')
+print(book)
+
+# set
+book = {1, 'learn python hard way', 'zed shaw', 2021, 'zed shaw'}
+print(book)
+
+# dict
 book = {
     'id': 1,
     'title': 'learn python hard way',
@@ -9,8 +21,9 @@ book = {
 }
 print(book)
 
-book = {1, 'learn python hard way', 'zed shaw', 2021}
-print(book)
 
-book = (1, 'learn python hard way', 'zed shaw', 2021)
-print(book)
+# ID,TITLE,AUTHOR,YEAR
+# ,,,
+# ,,,
+# 1,learn python hard way,zed shaw,2021
+# 2,,,
