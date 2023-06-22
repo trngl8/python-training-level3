d28b853bc3e298a301a1a06e34c64e6ca1afd2d9
Romаn Mоvchan <movchan@gmail.com>
2023-06-22 20:03:46 +0300

    menu changes, close #1 (#34)
    Co-authored-by: Red Rubber Duck <mvch@triangle.software>
    Co-authored-by: ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>

--- menu.py
+++ menu.py
@@ -1,7 +1,23 @@
-print("[1]. data structures (list, dict, tuple, set)")
-print("[2]. functions")
-print("[3]. filesystem operations")
-print("[4]. exceptions")
-print("[5]. objects details and abstractions")
-print("[6]. database")
-print("[7]. web application")
+menu_list = ["data structures (list, dict, tuple, set)" , "functions" ,"filesystem operations" , "exceptions" , "objects details and abstractions" ,"database" , "web application"]
+for i in range(len(menu_list)):
+    print(f"[{i+1}]. {menu_list[i]}")
+number = input("Enter your choice: ")
+try:
+    number = float(number)
+except :
+    print("You should enter a number")
+    exit()
+if str(number)[str(number).index('.') + 1] != "0" and len(str(number)) != str(number).index('.') + 1:
+    print('Number should be integer')
+    exit()
+if not 8 <= int(number) <=0:
+    print("Number should be from 1 to 7")
+    exit()
+result = menu_list[int(number)- 1]
+result = result.capitalize()
+print(result)
+
+# Expected input:
+# 7
+# Expected output:
+# Web application
